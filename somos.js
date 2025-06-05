682e1eda2f46784e49ba83f032bd376ab935ffe0
jorge2442 <a100244299@my.uvm.edu.mx>
2025-06-05 08:26:32 -0600

    somos.js

--- somos.js
+++ somos.js
@@ -4,6 +4,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const downloadBtn = document.querySelector('.download-btn');
 
     downloadBtn.addEventListener('click', function() {
-        alert('¡Gracias por descargar el PDF! Esperamos que sea útil.');
+        alert('¡Gracias por descargar el PDF! Esperamos que te sea útil en tu camino hacia una vida más saludable.');
     });
 });
